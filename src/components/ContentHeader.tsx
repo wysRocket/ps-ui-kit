56a092603e8e098af5230b0d4be30d1389a4d40f
Alexey Hodkov <alexey.hodkov@gmail.com>
2020-05-07 16:06:54 +0300

    fix v align

--- src/components/ContentHeader.tsx
+++ src/components/ContentHeader.tsx
@@ -13,20 +13,20 @@ export default class ContentHeader extends React.Component<IProps> {
     const style = this.props.style;
     const height = style && style.height ? style.height : 92;
     return(
-      <div style={{height}}>
-      <div style={{display: 'flex', position: 'relative', alignItems: 'center', height: 'auto'}}>
-        <div style={{display: 'inline-flex', position: 'relative', flexDirection: 'column', alignItems: 'center'}}>
-          {first}
+      <div style={{height, display: 'grid'}}>
+        <div style={{display: 'flex', position: 'relative', alignItems: 'center', height: 'auto'}}>
+          <div style={{display: 'inline-flex', position: 'relative', flexDirection: 'column', alignItems: 'center'}}>
+            {first}
+          </div>
+          <div style={{flexGrow: 1}} />
+          {children.map((child, index) => {
+            return (
+              <div key={index} style={{display: 'inline-flex', position: 'relative', flexDirection: 'column', alignItems: 'center'}}>
+                {child}
+              </div>
+            );
+          })}
         </div>
-        <div style={{flexGrow: 1}} />
-        {children.map((child, index) => {
-          return (
-            <div key={index} style={{display: 'inline-flex', position: 'relative', flexDirection: 'column', alignItems: 'center'}}>
-              {child}
-            </div>
-          );
-        })}
-      </div>
       </div>
     );
   }

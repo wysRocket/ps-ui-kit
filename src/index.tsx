0fab116d62f0e820aabd02acad8a30782f2599ba
Alexey Hodkov <alexey.hodkov@gmail.com>
2020-05-12 17:31:09 +0300

    filter export

--- src/index.tsx
+++ src/index.tsx
@@ -46,6 +46,7 @@ import {Attribute as IAttribute} from "./domain/Attribute";
 import {HGroup} from "./components/Group";
 import PopUp from "./components/PopUp";
 import {Tree, TreeContent as ITreeContent, TreeElement as ITreeElement} from "./components/tree/Tree";
+import Filter from "./components/Filter";
 
 export type Service = IService;
 export type Identity = IIdentity;
@@ -81,6 +82,7 @@ export {
   Paginator,
   IconPreview,
   TabbedPanel,
+  Filter,
   PopUp,
   Tree,
   HGroup,

2108b6c7bec372bb9d925f13b7a29d8551d7f508
Vladimir Kvirenko <wysness@gmail.com>
2022-09-12 19:57:16 +0300

    fixed small issues

--- src/components/AppHeader/AppHeader.stories.tsx
+++ src/components/AppHeader/AppHeader.stories.tsx
@@ -1,33 +1,35 @@
-import {Story} from "@storybook/react";
+import {ComponentMeta, ComponentStory} from "@storybook/react";
+import {UserRole} from "../../domain/User";
 import {AppHeader as AHeader, IProps} from "./AppHeader";
-
 
 export default {
   title: "Components/AppHeader",
   component: AHeader
-};
+} as ComponentMeta<typeof AHeader>;
 
-const Template: Story<IProps> = (args) => <AHeader {...args} />;
+const Template: ComponentStory<typeof AHeader> = (args) => <AHeader {...args} />;
 
-
-export const AppHeader: Story<IProps> = Template.bind({});
+export const AppHeader = Template.bind({});
 AppHeader.args = {
-  user: {login: 'Test Login', email: 'testEmail@email.com', roles: ['ZAKA_ADMIN'], 
-  User1: '2',},
+  user: {
+    login: "Test Login",
+    email: "testEmail@email.com",
+    roles: [UserRole.ZAKA_ADMIN],
+    User1: "2"
+  },
   menuItems: [
     {
-      onClick: () => console.log( "Charts click"),
-      label: 'Charts',
+      onClick: () => console.log("Charts click"),
+      label: "Charts"
       // icon?: any;
     },
     {
-      onClick: () => console.log( "Logout click"),
-      label: 'Logout',
+      onClick: () => console.log("Logout click"),
+      label: "Logout"
       // icon?: any;
     }
   ],
   onFilterChange: (filter: string) => console.log(filter),
-  filterPlaceholder: 'Filter placeholder',
-  onSwitchSidebarMode: () => console.log('onSwitchSidebarMode function'),
+  filterPlaceholder: "Filter placeholder",
+  onSwitchSidebarMode: () => console.log("onSwitchSidebarMode function")
 };
-
